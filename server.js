ce9bfbfc4ee9603eb0884e318afc2899667d6785
riannsttar <softtokito@gmail.com>
2024-10-04 22:23:17 +0000

    vvvvvv

--- server.js
+++ server.js
@@ -1,1 +1,17 @@
 import express from 'express'
+
+const app = express()
+
+const users = []
+
+app.post('/usuarios', (req, res) => {
+    users.push(req.body)
+
+    res.send(201)
+})
+
+app.get('/usuarios', (req, res) => {
+    res.send(200)
+})
+
+app.listen(3000)
