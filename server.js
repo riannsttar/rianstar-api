2c8d8078302f3fcfff0a105db0f031934579009b
riannsttar <softtokito@gmail.com>
2024-10-05 15:49:42 +0000

    vsabfd

--- server.js
+++ server.js
@@ -34,13 +34,26 @@ app.put('/usuarios/:id', async (req, res) => {
         where: {
             id: req.params.id
         },
-
         data: {
             email: req.body.email,
             name: req.body.name,
             age: req.body.age
         }
     })
+
+    res.status(201).json(req.body)
+})
+
+app.delete('usuarios/:id', async (req, res) => {
+    
+    await prisma.user.delete({
+        where: {
+            id: req.params.id
+        }
+    })
+
+    res.status(200).json({message:"Usuario deletado com sucesso!"})
+
 })
 
 app.listen(3000)
