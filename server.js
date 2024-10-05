e3d15f5cbbc3ede3d2dd6e19358daeba18bcd416
riannsttar <softtokito@gmail.com>
2024-10-05 13:36:06 +0000

    simboraaaaaaaaaaaaaaaaa

--- server.js
+++ server.js
@@ -1,17 +1,33 @@
 import express from 'express'
+import { PrismaClient } from '@prisma/client'
+
+const prisma = new PrismaClient()
 
 const app = express()
+app.use(express.json())
 
 const users = []
 
-app.post('/usuarios', (req, res) => {
-    users.push(req.body)
+app.post('/usuarios', async (req, res) => {
 
-    res.send(201)
+    await prisma.user.create({
+        data: {
+            email: req.body.email,
+            name: req.body.email,
+            age: req.body.age
+        }
+    })
+
+    res.status(201).json(req.body)
+
 })
 
-app.get('/usuarios', (req, res) => {
+app.get('/usuarios', async (req, res) => {
+
+    await prisma.user.findMany()
+
     res.send(200)
+
 })
 
 app.listen(3000)
